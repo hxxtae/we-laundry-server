80d3c2158f503166d73c372f7fa9a92d0ea60e8c
hxxtae <fkdlxmfkdl@gmail.com>
2022-05-03 23:53:04 +0900

    :book: [feat]: app.ts 수정 & 라이브러리 추가
    - express
    - express-rate-limit
    - express-validator
    - mongodb
    - bcrypt
    - jsonwebtoken
    - cors
    - cookie-parser

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+
+// ------------------------------
+// 환경변수 값 설정
+// ------------------------------
+
+dotenv.config(); // `.env` 파일 내용을 process.env(=환경변수)에 로드합니다.
+
+function required(key: string, defaultValue?: string) {
+  const value = process.env[key] || defaultValue;
+  if (value === undefined) {
+    throw new Error(`Key ${key} is undefined`);
+  }
+  return value;
+}
+
+export const config = {
+  jwt: {
+    secretKey: required('JWT_SECRET'),
+    expiresInSec: parseInt(required('JWT_EXPIRES_SEC', '86400')),
+  },
+  bcrypt: {
+    saltRounds: parseInt(required('BCRYPT_SALT_ROUNDS', '10')),
+  },
+  port: parseInt(required('PORT')),
+  mongo: {
+    host: required('MONGODB_URI'),
+  },
+  cors: {
+    allowedOrigin: required('CORS_ALLOW_ORIGIN'),
+  },
+  csrf: {
+    plainToken: required('CSRF_SECRET_KEY'),
+  },
+  rateLimit: {
+    windowMs: 60000,
+    maxRequest: 100,
+  }
+};
