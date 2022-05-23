692817f52a8f4e43de628cce09119ffa9eb481d5
hxxtae <fkdlxmfkdl@gmail.com>
2022-05-23 22:30:28 +0900

    :books: [Docs]: Refactory And Text Setting

--- src/controller/address.js
+++ src/controller/address.js
@@ -21,7 +21,7 @@ export async function createAddress(req, res, next) {
   const { addname, addfullname } = req.body;
   const found = await addressRepository.getByAddname(addname, req.userName)
   if (found) {
-    return res.status(409).json({ message: `${addname} 는 이미 사용중입니다.` });
+    return res.status(409).json({ message: `${addname} 는(은) 이미 사용중입니다.` });
   }
   const add = await addressRepository.create(addname, addfullname, req.userName);
   res.status(201).json(add); 
@@ -34,11 +34,17 @@ export async function createAddress(req, res, next) {
 export async function updateAddress(req, res, next) {
   const { addname, addfullname } = req.body;
   const id = req.params.id;
+
   const found = await addressRepository.getById(id, req.userName);
   if (!found) {
     return res.status(404).json({message: `Address id(${id}) not found`});
   }
-  
+
+  const found2 = await addressRepository.getByAddname(addname, req.userName)
+  if (found2 && (found.addname !== addname)) {
+    return res.status(409).json({ message: `${addname} 는(은) 이미 사용중입니다.` });
+  }
+
   const updated = await addressRepository.update(addname, addfullname, id, req.userName);
   res.status(200).json(updated);
 }
